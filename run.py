c2fc41876913a3290b6bdc43da5757886b63fcdb
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-03-25 03:57:18 +0000

    Add files via upload

--- run.py
+++ run.py
@@ -11,16 +11,15 @@ def downloadpack(url):
     zipfile = ZipFile(BytesIO(req.content))
     zipfile.extractall('pack/')
 
-downloadpack(os.environ.get("PACK_URL"))
 try:
     with open("pack/assets/minecraft/font/default.json", "r") as f:
         data = json.load(f)
         symbols = [d['chars'] for d in data['providers']]
         paths = [d['file'] for d in data['providers']]
 except:
-    exit()
+    sys.exit("No Have Font")
 
-def createfoler():
+def createfoler(glyph):
     if not os.path.exists(f"images/{glyph}"):
         os.makedirs(f"images/{glyph}")
     if not os.path.exists(f"export/{glyph}"):
@@ -47,6 +46,8 @@ def create_empty(glyph, blankimg):
             image.save(f"images/{glyph}/0x{glyph}{name}.png", "PNG")
 
 def imagetoexport(glyph, blankimg):
+    if os.path.isdir(f"export/{glyph}") == False:
+        os.mkdir(f"export/{glyph}")
     filelist = [file for file in os.listdir(f'images/{glyph}') if file.endswith('.png')]
     for img in filelist:
         image = Image.open(blankimg)
@@ -67,62 +68,74 @@ for i in symbols:
 glyphs = list(dict.fromkeys(glyphs))
 print(glyphs)
 
+listglyphdone = []
+    
 def converterpack(glyph):
+    if os.path.isdir(f"images/{glyph}") == False:
+        os.mkdir(f"images/{glyph}")
     if len(symbols) == len(paths):
         maxsw, maxsh = 0, 0
         for symboll, path in zip(symbols, paths):
             symbolbe = ''.join(symboll)
             symbolbehex = (hex(ord(symbolbe)))
-            symbol = symbolbehex[4:]
-            symbolac = symbolbehex[2:]
-            symbolcheck = symbolac[:2]
+            if glyph in listglyphdone:
+                return False
+            if len(symbolbehex) == 6:
+                symbol = symbolbehex[4:]
+                symbolac = symbolbehex[2:]
+                symbolcheck = symbolac[:2]
+            elif len(symbolbehex) == 5:
+                symbolbehex = symbolbehex[:2] + "0" + symbolbehex[2:]
+                symbol = symbolbehex[4:]
+                symbolac = symbolbehex[2:]
+                symbolcheck = symbolac[:2]
+                glyphs.append(symbolcheck.upper())
             if (symbolcheck.upper()) == (glyph.upper()):
+                print(symbolbehex)
                 if ":" in path:
                     try:
                         namespace = path.split(":")[0]
                         pathnew = path.split(":")[1]
                         imagefont = Image.open(f"pack/assets/{namespace}/textures/{pathnew}")
                         image = imagefont.copy()
-                        #os.remove(f"images/{glyph}/0x{glyph}{symbol}.png")
                         image.save(f"images/{glyph}/0x{glyph}{symbol}.png", "PNG")
-                        sw, sh = imagefont.size
-                        maxsw, maxsh = max(maxsw, sw), max(maxsh, sh)
                     except Exception as e:
-                        maxsw, maxsh = maxsw, maxsh
                         print(e)
-                        pass
+                        continue
                 else:
                     try:
                         imagefont = Image.open(f"pack/assets/minecraft/textures/{path}")
                         image = imagefont.copy()
-                        #os.remove(f"images/{glyph}/0x{glyph}{symbol}.png")
                         image.save(f"images/{glyph}/0x{glyph}{symbol}.png", "PNG")
-                        sw, sh = imagefont.size
-                        maxsw, maxsh = max(maxsw, sw), max(maxsh, sh)
                     except Exception as e: 
-                        maxsw, maxsh = maxsw, maxsh
                         print(e)
-                        pass
+                        continue
             else:
                 continue
-        else:
+        else:                
+            files = glob.glob(f"images/{glyph}/*.png")
+            for file in files:
+                image = Image.open(file)
+                sw, sh = image.size
+                maxsw, maxsh = max(maxsw, sw), max(maxsh, sh)
             if maxsw == maxsh:
-                size = maxsw, maxsh
+                size = maxsw, maxsw
             elif maxsw > maxsh:
                 size = maxsw, maxsw
             elif maxsh > maxsw:
                 size = maxsh, maxsh
-            glyphsize = size * 16
-            img = Image.open("blank256.png")
-            imgre = img.resize(size)
-            imgre.save("blankimg.png")
-            blankimg = "blankimg.png"
-            create_empty(glyph, blankimg) 
-            imagetoexport(glyph, blankimg)
-            sprite(glyph, glyphsize, size)
+            if size == (0, 0):
+                pass
+            else:
+                glyphsize = size * 16
+                img = Image.open("blank256.png")
+                imgre = img.resize(size)
+                imgre.save("blankimg.png")
+                blankimg = "blankimg.png"
+                create_empty(glyph, blankimg) 
+                imagetoexport(glyph, blankimg)
+                sprite(glyph, glyphsize, size)
+                listglyphdone.append(glyph)
             
 for glyph in glyphs:
-    try:
-        converterpack(glyph)
-    except:
-        pass
+    converterpack(glyph)
