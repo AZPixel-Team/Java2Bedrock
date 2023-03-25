c72f03a7b109b45ef8e9d706f7398ddade0c4f50
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-03-25 12:48:40 +0700

    Update sprite.py

--- sprite.py
+++ sprite.py
@@ -58,5 +58,5 @@ def sprite(glyph, spritesheet = None, tile = None):
         cut_frame = current_frame.crop((0,0,tile_width,tile_height))
     
         spritesheet.paste(cut_frame, box)
-    os.makedirs("target/rp/font", exist_ok = True)
+    os.makedirs("staging/target/rp/font", exist_ok = True)
     spritesheet.save(f"staging/target/rp/font/glyph_{glyph}.png", "PNG")
