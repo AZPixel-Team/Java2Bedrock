8c36d48722025222509da6120b249fe187ef91df
Hahaa13 <dangkida9@gmail.com>
2023-11-14 06:55:41 +0700

    FIX MEG3

--- meg3.py
+++ meg3.py
@@ -5,16 +5,20 @@ import os
 if os.getenv("ATTACHABLE_MATERIAL") == "entity_emissive_alpha_one_sided" and os.getenv("MEG3_FIX") == "true":
     files = glob.glob("staging/target/rp/attachables/modelengine/**/*.json")
     for file in files:
-        with open(file, "r") as f:
-            texture_file = json.load(f)["minecraft:attachable"]["description"]["textures"]["default"]
-            texture = f"staging/target/rp/{texture_file}"
-        im = Image.open(texture)
-        im = im.convert('RGBA')
-        sx, sy = im.size
-        im.putalpha(51)
-        imd = im.load()
-        for x in range(sx):
-            for y in range(sy):
-                if imd[x,y] == (0, 0, 0, 51):
-                    imd[x,y] = 0,0,0,0
-        im.save(texture)
+        try:
+            with open(file, "r") as f:
+                texture_file = json.load(f)["minecraft:attachable"]["description"]["textures"]["default"]
+                texture = f"staging/target/rp/{texture_file}"
+            im = Image.open(texture)
+            im = im.convert('RGBA')
+            sx, sy = im.size
+            im.putalpha(51)
+            imd = im.load()
+            for x in range(sx):
+                for y in range(sy):
+                    if imd[x,y] == (0, 0, 0, 51):
+                        imd[x,y] = 0,0,0,0
+            im.save(texture)
+        except Exception as e:
+            print(e)
+            print("Error texture:" + texture)
