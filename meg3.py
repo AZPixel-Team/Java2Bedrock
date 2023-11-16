6b72d557d4eba6c6ced4943c2dd85abd40d672b2
Hahaa13 <dangkida9@gmail.com>
2023-11-16 19:51:32 +0700

    Changes to be committed: 	modified:   meg3.py

--- meg3.py
+++ meg3.py
@@ -15,7 +15,7 @@ if os.getenv("ATTACHABLE_MATERIAL") == "entity_emissive_alpha_one_sided" and os.
                 A = im.getchannel("A")
                 alpha = A.point(lambda i: 51 if i>0 else 0)
                 im.putalpha(alpha)
-                im.save()
+                im.save(texture)
                 fdone.append(file)
             except Exception as e:
                 print(e)
