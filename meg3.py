483ac592f40f0eaca6543a71f695a10f7f8ec0ed
Hahaa13 <dangkida9@gmail.com>
2023-11-16 18:36:15 +0700

    test

--- meg3.py
+++ meg3.py
@@ -10,15 +10,10 @@ if os.getenv("ATTACHABLE_MATERIAL") == "entity_emissive_alpha_one_sided" and os.
                 texture_file = json.load(f)["minecraft:attachable"]["description"]["textures"]["default"]
                 texture = f"staging/target/rp/{texture_file}.png"
             im = Image.open(texture)
-            im = im.convert('RGBA')
-            sx, sy = im.size
-            im.putalpha(51)
-            imd = im.load()
-            for x in range(sx):
-                for y in range(sy):
-                    if imd[x,y] == (0, 0, 0, 51):
-                        imd[x,y] = (0,0,0,0)
-            im.save(texture)
+            A = im.getchannel("A")
+            alpha = A.point(lambda i: 51 if i>0 else 0)
+            im.putalpha(alpha)
+            im.save()
         except Exception as e:
             print(e)
             print("Error texture:" + texture)
