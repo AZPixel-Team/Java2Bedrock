ea89f051895422561b1b5a0272cc8998edafa717
Hahaa13 <dangkida9@gmail.com>
2023-11-20 10:21:01 +0700

    f

--- sound.py
+++ sound.py
@@ -23,15 +23,15 @@ if os.getenv("SOUNDS_CONVERSION") == "true":
                         try:
                             if type(sound).__name__  == "dict":
                                 sound["name"]
-                                b = (sound["name"].split(":")[-1] + ".ogg").split((sound["name"].split(":")[-1] + ".ogg").split("/")[-1])[0]
+                                b = (sound["name"].split(":")[-1] + ".ogg").split((sound['name'].split(':')[-1] + ".ogg").split("/")[-1])[0]
                                 sound["name"] = "sounds/" + sound["name"].split(":")[-1]
                                 os.makedirs("staging/target/rp/sounds/" + b, exist_ok=True)
-                                shutil.copyfile(f"pack/assets/minecraft/{sound["name"].split(":")[-1]}.ogg", f"staging/target/rp/{sound["name"].split(":")[-1]}.ogg")
+                                shutil.copyfile(f"pack/assets/minecraft/{sound['name'].split(':')[-1]}.ogg", f"staging/target/rp/{sound['name'].split(':')[-1]}.ogg")
                                 listsound.append(sound)
                             else:
-                                b = (sound.split(":")[-1] + ".ogg").split((sound.split(":")[-1] + ".ogg").split("/")[-1])[0]
+                                b = (sound.split(":")[-1] + ".ogg").split((sound.split(':')[-1] + ".ogg").split("/")[-1])[0]
                                 os.makedirs("staging/target/rp/sounds/" + b, exist_ok=True)
-                                shutil.copyfile(f"pack/assets/minecraft/sounds/{sound.split(":")[-1]}.ogg", f"staging/target/rp/sounds/{sound.split(":")[-1]}.ogg")
+                                shutil.copyfile(f"pack/assets/minecraft/sounds/{sound.split(':')[-1]}.ogg", f"staging/target/rp/sounds/{sound.split(':')[-1]}.ogg")
                                 a = "sounds/" + sound.split(":")[-1]
                                 listsound.append(a)
                         except Exception as e:
