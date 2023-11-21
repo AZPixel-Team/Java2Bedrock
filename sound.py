d4570e05a86501d2bb081a8c3db8802abd8ebac8
Hahaa13 <dangkida9@gmail.com>
2023-11-21 09:09:10 +0700

    f

--- sound.py
+++ sound.py
@@ -1,40 +1,40 @@
 import json, glob, os, shutil
-if os.getenv("SOUNDS_CONVERSION") == "true":
-    files = glob.glob("pack/assets/**/sounds.json")
-    os.makedirs("staging/target/rp/sounds", exist_ok=True)
-    with open("staging/target/rp/sounds/sound_definitions.json", "w") as f:
-        f.write('{"format_version": "1.14.0", "sound_definitions": {}}')
-    for file in files:
-        with open(file, "r") as f:
-            data = json.load(f)
-        names = [d for d in data]
-        with open("staging/target/rp/sounds/sound_definitions.json", "r") as f:
-            dj = json.load(f)
-            for name in names:
-                dj['sound_definitions'][f"{name}"] = {}
-                with open("staging/target/rp/sounds/sound_definitions.json", "w") as f:
+
+files = glob.glob("pack/assets/**/sounds.json")
+os.makedirs("staging/target/rp/sounds", exist_ok=True)
+with open("staging/target/rp/sounds/sound_definitions.json", "w") as f:
+    f.write('{"format_version": "1.14.0", "sound_definitions": {}}')
+for file in files:
+    with open(file, "r") as f:
+        data = json.load(f)
+    names = [d for d in data]
+    with open("staging/target/rp/sounds/sound_definitions.json", "r") as f:
+        dj = json.load(f)
+        for name in names:
+            dj['sound_definitions'][f"{name}"] = {}
+            with open("staging/target/rp/sounds/sound_definitions.json", "w") as f:
+                try:
+                    dj['sound_definitions'][f"{name}"]["category"] = data[name]["category"]
+                except:
+                    dj['sound_definitions'][f"{name}"]["category"] = "neutral"
+                sounds = data[name]["sounds"]
+                listsound = []
+                for sound in sounds:
                     try:
-                        dj['sound_definitions'][f"{name}"]["category"] = data[name]["category"]
-                    except:
-                        dj['sound_definitions'][f"{name}"]["category"] = "neutral"
-                    sounds = data[name]["sounds"]
-                    listsound = []
-                    for sound in sounds:
-                        try:
-                            if type(sound).__name__  == "dict":
-                                sound["name"]
-                                b = (sound["name"].split(":")[-1] + ".ogg").split((sound['name'].split(':')[-1] + ".ogg").split("/")[-1])[0]
-                                sound["name"] = "sounds/" + sound["name"].split(":")[-1]
-                                os.makedirs("staging/target/rp/sounds/" + b, exist_ok=True)
-                                shutil.copyfile(f"pack/assets/minecraft/{sound['name'].split(':')[-1]}.ogg", f"staging/target/rp/{sound['name'].split(':')[-1]}.ogg")
-                                listsound.append(sound)
-                            else:
-                                b = (sound.split(":")[-1] + ".ogg").split((sound.split(':')[-1] + ".ogg").split("/")[-1])[0]
-                                os.makedirs("staging/target/rp/sounds/" + b, exist_ok=True)
-                                shutil.copyfile(f"pack/assets/minecraft/sounds/{sound.split(':')[-1]}.ogg", f"staging/target/rp/sounds/{sound.split(':')[-1]}.ogg")
-                                a = "sounds/" + sound.split(":")[-1]
-                                listsound.append(a)
-                        except Exception as e:
-                            print(e)
-                    dj["sound_definitions"][f"{name}"]["sounds"] = listsound
-                    json.dump(dj, f, indent=2)
+                        if type(sound).__name__  == "dict":
+                            sound["name"]
+                            b = (sound["name"].split(":")[-1] + ".ogg").split((sound['name'].split(':')[-1] + ".ogg").split("/")[-1])[0]
+                            sound["name"] = "sounds/" + sound["name"].split(":")[-1]
+                            os.makedirs("staging/target/rp/sounds/" + b, exist_ok=True)
+                            shutil.copyfile(f"pack/assets/minecraft/{sound['name'].split(':')[-1]}.ogg", f"staging/target/rp/{sound['name'].split(':')[-1]}.ogg")
+                            listsound.append(sound)
+                        else:
+                            b = (sound.split(":")[-1] + ".ogg").split((sound.split(':')[-1] + ".ogg").split("/")[-1])[0]
+                            os.makedirs("staging/target/rp/sounds/" + b, exist_ok=True)
+                            shutil.copyfile(f"pack/assets/minecraft/sounds/{sound.split(':')[-1]}.ogg", f"staging/target/rp/sounds/{sound.split(':')[-1]}.ogg")
+                            a = "sounds/" + sound.split(":")[-1]
+                            listsound.append(a)
+                    except Exception as e:
+                        print(e)
+                dj["sound_definitions"][f"{name}"]["sounds"] = listsound
+                json.dump(dj, f, indent=2)
