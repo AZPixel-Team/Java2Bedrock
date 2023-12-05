700f92dbe05df97b27c42939753f6f9850720434
Hahaa13 <dangkida9@gmail.com>
2023-12-05 18:55:19 +0700

    THE F

--- bow.py
+++ bow.py
@@ -81,4 +81,5 @@ for file in files:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         print(exc_type, fname, exc_tb.tb_lineno)
+        print(3)
 Bow_Util.acontroller(gmdllist)
