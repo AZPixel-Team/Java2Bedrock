674d94caf506c28494a51b1d2431b2cdd0d584d7
Hahaa13 <dangkida9@gmail.com>
2023-12-13 18:19:15 +0700

    FIX STUPID

--- manager.py
+++ manager.py
@@ -2,13 +2,19 @@ import zipfile, os
 
 with zipfile.ZipFile("staging/input_pack.zip", "r") as file:
     file.extractall("pack/")
-if os.getenv("SOUNDS_CONVERSION") == "true":
-    import sound
-if os.getenv("ATTACHABLE_MATERIAL") == "entity_emissive_alpha_one_sided" and os.getenv("MEG3_FIX") == "true":
-    import meg3
-if os.getenv("ARMOR_CONVERSION") == "true":
-    import armor
-if os.getenv("FONT_CONVERSION") == "true":
-    import font
-if os.getenv("BOW_CONVERSION") == "true":
-    import bow
+
+try: 
+    if os.getenv("SOUNDS_CONVERSION") == "true": import sound
+except Exception as e: print(e)
+try:
+    if os.getenv("ATTACHABLE_MATERIAL") == "entity_emissive_alpha_one_sided" and os.getenv("MEG3_FIX") == "true": import meg3
+except Exception as e: print(e)
+try:
+    if os.getenv("ARMOR_CONVERSION") == "true": import armor
+except Exception as e: print(e)
+try:
+    if os.getenv("FONT_CONVERSION") == "true": import font
+except Exception as e: print(e)
+try:
+    if os.getenv("BOW_CONVERSION") == "true": import bow
+except Exception as e: print(e)
