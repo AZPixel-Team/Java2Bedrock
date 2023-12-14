af875d311c3cd0af6d92071c1ec0000418081fd5
Hahaa13 <dangkida9@gmail.com>
2023-12-14 07:03:26 +0700

    F

--- meg3.py
+++ meg3.py
@@ -3,23 +3,23 @@ import json
 import glob
 import os
 files = glob.glob("staging/target/rp/attachables/modelengine/**/*.json")
-fdone = []
+texture_done = []
 for file in files:
-    if not file in fdone:
-        try:
-            with open(file, "r") as f:
-                texture_file = json.load(f)["minecraft:attachable"]["description"]["textures"]["default"]
-                texture = f"staging/target/rp/{texture_file}.png"
-            im = Image.open(texture).convert("RGBA")
-            im.putalpha(51)
-            pixels = im.load()
-            for x in range(im.width):
-                for y in range(im.height):
-                    if pixels[x,y] == (0,0,0,51):
-                        pixels[x,y] = (0,0,0,0)
-            im.save(texture)
-            fdone.append(file)
-        except Exception as e:
-            print(e)
-            print("Error texture:" + texture)
-print(fdone)
+    try:
+        with open(file, "r") as f:
+            texture_file = json.load(f)["minecraft:attachable"]["description"]["textures"]["default"]
+            texture = f"staging/target/rp/{texture_file}.png"
+            if texture_file in texture_done: continue
+        im = Image.open(texture).convert("RGBA")
+        im.putalpha(51)
+        pixels = im.load()
+        for x in range(im.width):
+            for y in range(im.height):
+                if pixels[x,y] == (0,0,0,51):
+                    pixels[x,y] = (0,0,0,0)
+        im.save(texture)
+        texture_done.append(texture_file)
+    except Exception as e:
+        print(e)
+        print("Error texture:" + texture)
+print(texture_done)
