17bbfa0e0386cc5a207eac9e3fc98ca93810d2ef
Hahaa13 <ertdgh123@gmail.com>
2023-12-24 08:14:50 +0700

    FIX SHIELD

--- shield.py
+++ shield.py
@@ -8,19 +8,24 @@ if os.path.exists("pack/assets/minecraft/models/item/shield.json"):
         predicate = [d["predicate"] for d in data["overrides"]]
         model = [d["model"] for d in data["overrides"]]
     for m, p in zip(model, predicate):
-        fpath = (f"cache/shield/{p['custom_model_data']}.json")
-        if not os.path.exists(fpath):
-            os.makedirs(os.path.dirname(fpath), exist_ok=True)
+        if m == "item/shield" or "custom_model_data" not in p: continue
+        try:
+            fpath = (f"cache/shield/{p['custom_model_data']}.json")
+            if not os.path.exists(fpath):
+                os.makedirs(os.path.dirname(fpath), exist_ok=True)
+                with open(fpath, "w") as f:
+                    f.write("{}")
+            with open(fpath, "r") as f:
+                data = json.load(f)
             with open(fpath, "w") as f:
-                f.write("{}")
-        with open(fpath, "r") as f:
-            data = json.load(f)
-        with open(fpath, "w") as f:
-            if ("blocking" in p): data["blocking"] = m
-            else: data["default"] = m
-            if "check" in data: data["check"] = data["check"] + 1
-            else: data["check"] = 1
-            json.dump(data, f, indent=2)
+                if ("blocking" in p): data["blocking"] = m
+                else: data["default"] = m
+                if "check" in data: data["check"] = data["check"] + 1
+                else: data["check"] = 1
+                json.dump(data, f, indent=2)
+        except Exception as e:
+            print(e)
+            pass
 
 for file in glob.glob("cache/shield/*.json"):
     with open(file, "r") as f:
@@ -38,30 +43,30 @@ for file in glob.glob("cache/shield/*.json"):
                 dataA = json.load(f)
                 animationitem = dataA["minecraft:attachable"]["description"]["animations"]
                 gmdl = dataA["minecraft:attachable"]["description"]["identifier"]
-                if i == "default":
-                    saf = fa
-                    adata = dataA
-                    animation["mainhand.first_person"] = animationitem["firstperson_main_hand"]
-                    animation["mainhand.thierd_person"] = animationitem["thirdperson_main_hand"]
-                    animation["offhand.first_person"] = animationitem["firstperson_off_hand"]
-                    animation["offhand.thierd_person"] = animationitem["thirdperson_off_hand"]
-                    animate = [
-                        {"mainhand.thierd_person.block": f"!c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && query.is_sneaking"},
-                        {"mainhand.first_person.block": f"c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && query.is_sneaking"},
-                        {"mainhand.first_person": f"c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && !query.is_sneaking"},
-                        {"mainhand.thierd_person": f"!c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && !query.is_sneaking"},
+            if i == "default":
+                saf = fa
+                adata = dataA
+                animation["mainhand.first_person"] = animationitem["firstperson_main_hand"]
+                animation["mainhand.thierd_person"] = animationitem["thirdperson_main_hand"]
+                animation["offhand.first_person"] = animationitem["firstperson_off_hand"]
+                animation["offhand.thierd_person"] = animationitem["thirdperson_off_hand"]
+                animate = [
+                    {"mainhand.thierd_person.block": f"!c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && query.is_sneaking"},
+                    {"mainhand.first_person.block": f"c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && query.is_sneaking"},
+                    {"mainhand.first_person": f"c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && !query.is_sneaking"},
+                    {"mainhand.thierd_person": f"!c.is_first_person && c.item_slot == 'main_hand' && q.is_item_name_any('slot.weapon.mainhand', '{gmdl}') && !query.is_sneaking"},
 
-                        {"offhand.thierd_person.block": f"!c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && query.is_sneaking"},
-                        {"offhand.first_person.block": f"c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && query.is_sneaking"},
-                        {"offhand.first_person": f"c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && !query.is_sneaking"},
-                        {"offhand.thierd_person": f"!c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && !query.is_sneaking"}
-                    ]
-                else:
-                    animation["mainhand.first_person.block"] = animationitem["firstperson_main_hand"]
-                    animation["mainhand.thierd_person.block"] = animationitem["thirdperson_main_hand"]
-                    animation["offhand.first_person.block"] = animationitem["firstperson_off_hand"]
-                    animation["offhand.thierd_person.block"] = animationitem["thirdperson_off_hand"]
-                    os.remove(fa)
+                    {"offhand.thierd_person.block": f"!c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && query.is_sneaking"},
+                    {"offhand.first_person.block": f"c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && query.is_sneaking"},
+                    {"offhand.first_person": f"c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && !query.is_sneaking"},
+                    {"offhand.thierd_person": f"!c.is_first_person && c.item_slot == 'off_hand' && q.is_item_name_any('slot.weapon.offhand', '{gmdl}') && !query.is_sneaking"}
+                ]
+            else:
+                animation["mainhand.first_person.block"] = animationitem["firstperson_main_hand"]
+                animation["mainhand.thierd_person.block"] = animationitem["thirdperson_main_hand"]
+                animation["offhand.first_person.block"] = animationitem["firstperson_off_hand"]
+                animation["offhand.thierd_person.block"] = animationitem["thirdperson_off_hand"]
+                os.remove(fa)
         with open(saf, "w") as f:
             adata["minecraft:attachable"]["description"]["animations"] = animation
             adata["minecraft:attachable"]["description"]["scripts"]["animate"] = animate
